f75f6184f0901ff2af0ef964f81f262f36879ebe
Catherine Sarte <78393831+CSarte@users.noreply.github.com>
2023-12-20 12:55:02 -0500

    Update sketch comments

--- sketch_nov21a.ino
+++ sketch_nov21a.ino
@@ -1,12 +1,19 @@
 
+/**Title: Robotics Rubik's cube solvers
+ * Course Number: Robotics
+ * Date: 11/21/2023
+ * Author: Catherine Sarte, Joshua Anderson
+ * 
+ * Project Description: This program operates with the Python rubiks cube solver code to communicate with arduino uno 3 and rotate stepper motors to turn/solve the rubik cube
+ */
 
-// yellow and white pins at the top since i wont be doing any shifting with them
+//Pin settings
 const int yellowStepPin = 2;
 const int yellowDirPin = 3;
 const int whiteStepPin = 4;
 const int whiteDirPin = 5;
-// to reuse certain algorithm methods, i have these pins set up in ascending order
-// so i can dynamically find pins to the left and right of the main pin im using
+// to reuse certain algorithm methods, I have these pins set up in ascending order
+// so i can dynamically find pins to the left and right of the main pin I'm using
 // as a center piece for the algorithm
 int redStepPin = 6;
 int redDirPin = 7;
@@ -17,6 +24,8 @@ int orangeDirPin = 11;
 int blueStepPin = 12;
 int blueDirPin = 13;
 const int stepsPerRevolution = 200;
+
+//Set up each pin as output
 void setup() {
   Serial.begin(9600);
   pinMode(yellowStepPin, OUTPUT);
@@ -32,6 +41,8 @@ void setup() {
   pinMode(blueStepPin, OUTPUT);
   pinMode(blueDirPin, OUTPUT);
 }
+
+//function to turn side 90 degrees Clock wise
 void spin90CW(int motorPin, int DirPin) {
   digitalWrite(DirPin, HIGH);
 
@@ -42,6 +53,8 @@ void spin90CW(int motorPin, int DirPin) {
     delayMicroseconds(750);
   }
 }
+
+//function to turn side 90 degrees Counter Clock wise
 void spin90CCW(int motorPin, int DirPin) {
   
   for(int x = 0; x < (90 * stepsPerRevolution / 360); x++) {
@@ -52,6 +65,8 @@ void spin90CCW(int motorPin, int DirPin) {
   }
   
 }
+
+// Turns side 180 degrees clockwise if you wish to optimize the code
 void spin180(int motorPin) {
   for(int x = 0; x < (90 * stepsPerRevolution / 360); x++) {
     digitalWrite(motorPin,LOW);
@@ -61,18 +76,21 @@ void spin180(int motorPin) {
   }
 }
 
-
+//Main loop that will collect to solution path and turn the cube face
 void loop() {
    if (Serial.available() > 0) {
     int index = 0; // Index to track position in the buffer
     // Read characters until newline '\n' or maximum length is reached
     while (true) {
+      //If there is a character to be read enter
       if (Serial.available()) {
-        
+        //Read character in serial sent from the python code
         char incomingChar = Serial.read();
+        //Once the serial reach \n exit the loop because the solution path has ended
         if (incomingChar == '\n') {
           break; // Exit loop on newline
         }
+        // Else turn the face and direction that corresponds to the given solution letter
         else if(incomingChar == 'B'){ //flip yellow
           spin90CCW(yellowStepPin, yellowDirPin);
           delay(250);
